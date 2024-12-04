85e5fa35145f9ddb815a460afa590a2d631aa8b5
Alexander Leino <Alexander.Leino@yahoo.com>
2024-12-04 16:49:04 -0500

    created Roles: factory, controller, and routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 use App\Http\Controllers\HobbyController;
+use App\Http\Controllers\RolesController;
+use App\Models\Roles;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/hobby', [HobbyController::class, 'createHobby'])->name('hobby');
+
+Route::post('/roles', [RolesController::class, "createRole"])->name("roles");
