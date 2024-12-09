7ac14e92bc17264a5201fa3553028b8a2855266b
Alexander Leino <Alexander.Leino@yahoo.com>
2024-12-09 10:35:25 -0500

    started implementation of membership controller

--- app/Models/Membership.php
+++ app/Models/Membership.php
@@ -5,5 +5,21 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 
 class Membership extends Pivot
 {
-    
+    protected $table = "memberships";
+
+    protected $primaryKey = ['user_id', 'group_id'];
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $fillable = ['user_id', 'group_id', 'giftee_id', 'gift_id'];
+
+    protected function casts():array 
+    {
+        return [
+            'user_id' => $this->user_id,
+             'group_id' => $this->group_id, 
+             'giftee_id' => $this->giftee_id, 
+             'gift_id' => $this->gift_id,
+        ];
+    }
 }
