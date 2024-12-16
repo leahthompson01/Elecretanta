cb24a1f90829c0a669afddf8ceca881f6b234864
Richard <richardchoi54@gmail.com>
2024-12-16 14:29:49 -0500

    text input component

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -21,7 +21,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600 ' +
+                'text-[#B88914] rounded-[1000px] bg-[#F2E8CF] border-black border-solid	shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700' +
                 className
             }
             ref={localRef}
